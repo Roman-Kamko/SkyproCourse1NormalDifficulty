f844bf9b38aa83e4bf496e50456b25de2d00bf38
RomanKamko <r.kamko@mail.ru>
2023-03-20 10:39:27 +0400

    task 1.1

--- src/edu/skypro/homework/normaldifficulty/lesson1/Main.java
+++ src/edu/skypro/homework/normaldifficulty/lesson1/Main.java
@@ -2,6 +2,29 @@ package edu.skypro.homework.normaldifficulty.lesson1;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+    }
+
+    private static void task1() {
+        var dog = 8.0;
+        System.out.println(dog);
+        var cat = 3.6;
+        System.out.println(cat);
+        var paper = 763_789;
+        System.out.println(paper);
+    }
+    private static void task2() {
+
+    }
+    private static void task3() {
+    }
+    private static void task4() {
+    }
+    private static void task5() {
+    }
+    private static void task6() {
+    }
+    private static void task7() {
+    }
+    private static void task8() {
     }
 }
